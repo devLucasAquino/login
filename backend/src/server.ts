3b10716ca431901c19fa2a2312c7bd80949bf050
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-12-31 17:19:57 -0300

    feat: register user by front

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,12 @@
 import { fastify } from 'fastify';
 import { routes } from '../routes';
+import cors from '@fastify/cors';
 
 const app = fastify();
+
+app.register(cors, {
+    origin: '*',
+});
 
 app.register(routes);
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,42 @@
-function App() {
+import axios from "axios";
+import { useState } from "react";
+
+interface createUsers {
+  name: string;
+  email: string;
+  password: string;
+};
+
+
+const createUser = async ({name, email, password}: createUsers) => {
+  if(!name || !email || !password) return
+
+  axios.post("http://localhost:3333/user", {
+    name, email, password
+  })
+  .then((json) => console.log(json.data))
+  .catch((err) => console.log("error: \n", err))
+};
+
+export function App() {
+
+  const [ name, setName ] = useState<string>("");
+  const [ email, setEmail ] = useState<string>("");
+  const [ password, setPassword ] = useState<string>("");
+
+  const handleForm = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const user = {
+      name, email, password
+    };
+
+    createUser(user);
+  };
 
   return (
     <main className="flex justify-center items-center h-screen">
-      <form className="bg-white w-[300px] h-auto p-5 flex flex-col justify-center rounded-sm">
+      <form onSubmit={handleForm} className="bg-white w-[300px] h-auto p-5 flex flex-col justify-center rounded-sm">
         <h1 className="text-center text-2xl font-medium mb-5 text-pink-900">CADASTRE-SE</h1>
 
         <div className="flex flex-col gap-3">  
@@ -10,6 +44,8 @@ function App() {
             <label>Nome: </label>
             <input type="text" 
               placeholder="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="
                 px-2 py-1
                 border-pink-800 border-[3px] rounded-md
@@ -19,6 +55,8 @@ function App() {
             <label>E-mail: </label>
             <input type="text" 
               placeholder="e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="
                 px-2 py-1
                 border-pink-800 border-[3px] rounded-md
@@ -28,6 +66,8 @@ function App() {
             <label>Senha: </label>
             <input type="text" 
               placeholder="senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="
                 px-2 py-1
                 border-pink-800 border-[3px] rounded-md
@@ -36,6 +76,7 @@ function App() {
         </div>
 
         <button 
+          type="submit"
           className="mt-5 px-5 py-2 font-bold
                     bg-pink-600 text-slate-100 rounded-md transition-all
                     hover:bg-pink-700 hover:text-white hover:text-xl">
@@ -46,5 +87,3 @@ function App() {
     </main>
   )
 }
-
-export default App
